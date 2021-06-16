672fa184beaeb3129ca901d12023f3be0d642d6f
Gustavo <gguimp@gmail.com>
2021-06-16 11:41:28 -0300

    Adiciona refresh-token

--- auth/auth.routes.config.ts
+++ auth/auth.routes.config.ts
@@ -4,6 +4,7 @@ import authMiddleware from "./middleware/auth.middleware";
 import express from 'express';
 import bodyValidationMiddleware from "../common/middleware/body.validation.middleware";
 import { body } from 'express-validator';
+import jwtMiddleware from "./middleware/jwt.middleware";
 
 export class AuthRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
@@ -16,6 +17,13 @@ export class AuthRoutes extends CommonRoutesConfig {
             body('password').isString(),
             bodyValidationMiddleware.verifyBodyFieldsErrors,
             authMiddleware.verifyUserPassword,
+            authController.createJWT,
+        ]);
+
+        this.app.post('/auth/refresh-token', [
+            jwtMiddleware.validJWTNeeded,
+            jwtMiddleware.verifyRefreshBodyField,
+            jwtMiddleware.validRefreshNeeded,
             authController.createJWT,
         ]);
 
